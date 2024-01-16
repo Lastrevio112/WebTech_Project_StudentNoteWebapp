6268147c34369016a207f19f278848881586c609
Cristian Cojocarita <cojocaritacristian21@stud.ase.ro>
2024-01-16 08:53:11 +0200

    Starting point with basic setup

---
+++ client/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Note Taking App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
